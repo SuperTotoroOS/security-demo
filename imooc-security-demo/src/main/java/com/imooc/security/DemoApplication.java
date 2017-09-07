a36ea5bca584e5d136a4ead172c9e74000e53db3
杨蕴琦 <yunqi.yang@shijue.me>
2017-09-07 12:54:15 +0800

    初始化工程项目

---
+++ imooc-security-demo/src/main/java/com/imooc/security/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.imooc.security;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
